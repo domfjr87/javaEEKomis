0ac095e84dd4a2446e0c4382718efd324ee13794
dominikfjr87 <dominikfjr@gmail.com>
2015-06-06 11:51:07 +0200

    moreJPA

---
+++ komisSamochodowy-ejb/src/main/java/com/dto/PurchaseCar.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dto;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Dominik
+ */
+@Entity
+@Table(name="PURCHASE_CAR", schema="PROJEKT")
+public class PurchaseCar implements Serializable {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    int id;
+    @ManyToOne @JoinColumn(name = "ID_PRODUCT", referencedColumnName = "ID")
+    private CarDTO product;
+    @ManyToOne @JoinColumn(name = "ID_PURCHASE", referencedColumnName = "ID")
+    private PurchaseDTO purchase;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public CarDTO getProduct() {
+        return product;
+    }
+
+    public void setProduct(CarDTO product) {
+        this.product = product;
+    }
+
+    public PurchaseDTO getPurchase() {
+        return purchase;
+    }
+
+    public void setPurchase(PurchaseDTO purchase) {
+        this.purchase = purchase;
+    }
+    
+}
