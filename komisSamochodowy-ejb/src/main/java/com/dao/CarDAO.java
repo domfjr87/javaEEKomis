33907cf9320bd4d5c0e7bccbee2a9a8cf014d85c
dominikfjr87 <dominikfjr@gmail.com>
2015-06-06 12:47:21 +0200

    DAO interfaces

---
+++ komisSamochodowy-ejb/src/main/java/com/dao/CarDAO.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dao;
+import com.dto.CarDTO;
+/**
+ *
+ * @author Dominik
+ */
+public interface CarDAO extends GenericDao<CarDTO,Integer> {
+    
+}
---
+++ komisSamochodowy-ejb/src/main/java/com/dao/CustomerDAO.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dao;
+import com.dto.CustomerDTO;
+/**
+ *
+ * @author Dominik
+ */
+public interface CustomerDAO extends GenericDao<CustomerDTO, Integer> {
+    
+}
